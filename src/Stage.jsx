0e24e87e505dd77e70266a3e1b89d7a6c9271e97
jxnblk <jacksonblack@gmail.com>
2015-04-27 19:34:46 -0400

    Make paused state component

--- src/Stage.jsx
+++ src/Stage.jsx
@@ -9,6 +9,7 @@ var Fx = require('./Fx.jsx');
 var Science = require('./Science.jsx');
 var Chords = require('./Chords.jsx');
 var StepVisualizer = require('./StepVisualizer.jsx');
+var Paused = require('./Paused.jsx');
 var Icon = require('react-geomicons');
 
 var Stage = React.createClass({
@@ -59,32 +60,7 @@ var Stage = React.createClass({
             length={32} />
         </div>
         <div className="flex flex-center center" style={styles.paused}>
-          <div className="mx-auto">
-            <button className="caps mx-auto button button-big button-transparent"
-              onClick={this.props.playPause}>
-              <Icon name="play" className="h0" />
-            </button>
-            <div>
-              <a href="//github.com/jxnblk/skullcat"
-                className="m1 button button-outline">
-                GitHub
-              </a>
-              <a href="#!"
-                className="m1 button button-outline">
-                Tweet
-              </a>
-            </div>
-            <div>
-              <a href="/skullcat/1"
-                className="m1 button button-transparent">
-                Original Version
-              </a>
-              <a href="/skullcat/guidelines"
-                className="m1 button button-transparent">
-                Original Version
-              </a>
-            </div>
-          </div>
+          <Paused {...this.props} />
         </div>
         <div className="flex flex-center center" style={styles.play}>
           <button className="caps mx-auto button button-big button-transparent"
