28968938c5654386aab52a1823385bd9384e951d
jxnblk <jacksonblack@gmail.com>
2016-04-18 13:32:43 -0400

    Adjust electron setup

--- index.js
+++ index.js
@@ -22,13 +22,13 @@ app.on('window-all-closed', function() {
 // initialization and is ready to create browser windows.
 app.on('ready', function() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({ width: 800, height: 600 })
+  mainWindow = new BrowserWindow({ width: 1280, height: 960 })
 
   // and load the index.html of the app.
   mainWindow.loadURL('file://' + __dirname + '/index.html')
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+  // mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function() {
